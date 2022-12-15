cbe53e61dc36f5813ab74708dc015f200abecd86
Aly Ghazal <ali.ahmed2775@gmail.com>
2022-12-15 17:38:59 +0200

    CreateDB v1 check first char is number Fixed

--- CreateDB.sh
+++ CreateDB.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo "note that the name of the Database cannot contain spaces or any special character "
+echo "NOTE that the name of the Database cannot contain spaces, any special character or starts with number"
 read  -p "now enter the name of your new Database: " name
 source ./commonFunctions.sh
+
 if [[ $name = *" "*  ]]; then
         echo "we can't create a database its name contains spaces "
 else 
     if [[ `checkForSpecialCharacter $name` = 1 ]]; then
         echo "we can't create a database its name contains special characters "
     else
-        if [[ -d $name ]]; then
-            echo "this Database already exist"
+        if [[ `checkFirstChar $name` = 1 ]]; then
+            echo "we can't create a database its name starts with number "
         else
-            mkdir $name
-            echo "$name DB has been created"
+            if [[ -d $name ]]; then
+                echo "this Database already exist"
+            else
+                mkdir $name
+                echo "$name DB has been created"
+            fi
         fi
     fi
 fi
--- commonFunctions.sh
+++ commonFunctions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 shopt -s extglob
+#check for special character
 checkForSpecialCharacter(){
     if [[ $1 = +(*['!'@#\$%^\&*()_+]*) ]]; then        
         echo 1
@@ -17,3 +18,11 @@ checkSpaces(){
     fi
 }
 '
+#check if first char is number
+checkFirstChar(){
+    if [[ ${1:0:1} = [0-9] ]]; then
+        echo 1
+    else
+        echo 0
+    fi
+}
