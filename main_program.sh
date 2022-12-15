68ec4377d885ed3c56a1b535e1de328507fe7939
Aly Ghazal <ali.ahmed2775@gmail.com>
2022-12-15 13:36:46 +0200

    Making of the main menu

--- main_program.sh
+++ main_program.sh
@@ -4,3 +4,25 @@
 # [2] Tables
 # [3] column
 # [4] Data inside columns
+echo "Welcome to BasicDB where you can insert, delete, create, update --> tables & Databases "
+echo "please choose from the next list by number what do you want to do :) "
+select i in "Create Database" "Show all Databases" "connect to Database" "delete a Database"
+do
+case i in
+    "Create Database" )
+        . ./CreateDB.Sh
+    ;;
+    "Show all Databases" )
+        ls -F|grep "/"
+    ;;
+    "connect to Database" )
+        . ./ConnectDB.sh
+    ;;
+    "delete a Database" )
+        . ./deleteDB.sh
+    ;;
+    * )
+        echo "unavilable option"
+        . ./main_program.sh
+esac
+done
